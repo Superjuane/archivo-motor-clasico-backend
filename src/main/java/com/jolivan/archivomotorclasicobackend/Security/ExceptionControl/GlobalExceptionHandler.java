87cc468ea50903a10143624450bbd0dc9709d64b
Superjuane <juan.emilio.olivan@estudiantat.upc.edu>
2024-04-16 16:49:31 +0200

    Auth Exception Handler

---
+++ src/main/java/com/jolivan/archivomotorclasicobackend/Security/ExceptionControl/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.jolivan.archivomotorclasicobackend.Security.ExceptionControl;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Object> handleDataIntegrityViolation(DataIntegrityViolationException ex) {
+        String message = "The operation could not be completed due to an integrity constraint violation.";
+
+
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(message);
+    }
+
+}
